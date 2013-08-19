1725b7c2ea51832f18db0b103e48dd959c9c7275
Manfred Endres <manfred.endres@tslarusso.de>
2013-08-19 17:25:40 +0200

    Initial Commit

---
+++ LuaMeadow/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LuaMeadow
+//
+//  Created by Larusso on 19.08.13.
+//  Copyright (c) 2013 Larusso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
