a80f50c1b1ba38c9e6d3078e1d7a847cdaec78cc
Manfred Endres <manfred.endres@tslarusso.de>
2013-08-25 12:31:04 +0200

    update project to tutorial state 2

--- LuaMeadow/luascript.lua
+++ LuaMeadow/luascript.lua
@@ -1,11 +1,13 @@
-print("Hello from Lua")
-print("Lua code is capable of reading the value set from C++", cppvar)
-luavar = cppvar * 3
-
-function myluafunction(times)
-  return string.rep("(-)", times) 
+function entries(arg) -- iterator
+return function()
+return arg:pop();
+end
 end
 
-function myfunction(arg)
-  return cppfunction(arg)
+for i in entries(the_list) do
+io.write("From LUA:  ", i, "\n")
+end
+
+for i=1,10 do
+the_list:push(50+i*100);
 end
--- LuaMeadow/main.cpp
+++ LuaMeadow/main.cpp
@@ -1,67 +1,97 @@
-#include <iostream>
 #include <lua.hpp>
-
+#include <iostream>
+#include <list>
+#include <assert.h>
 
 extern "C" {
-  static int l_cppfunction(lua_State *L) {
-    double arg = luaL_checknumber(L,1);
-    lua_pushnumber(L, arg * 0.5);
-    return 1;
-  }
+    static int l_list_push(lua_State *L) { // Push elements from LUA
+        assert(lua_gettop(L) == 2); // check that the number of args is exactly 2
+        std::list<int> **ud = static_cast<std::list<int> **>(luaL_checkudata(L,1, "ListMT")); // first arg is the list
+        int v =luaL_checkint(L,2); // seconds argument is the integer to be pushed to the std::list<int>
+        (*ud)->push_back(v); // perform the push on C++ object through the pointer stored in user data
+        return 0; // we return 0 values in the lua stack
+    }
+    static int l_list_pop(lua_State *L) {
+        assert(lua_gettop(L) == 1); // check that the number of args is exactly 1
+        std::list<int> **ud = static_cast<std::list<int> **>(luaL_checkudata(L, 1, "ListMT")); // first arg is the userdata
+        if ((*ud)->empty()) {
+            lua_pushnil(L);
+            return 1; // if list is empty the function will return nil
+        }
+        lua_pushnumber(L,(*ud)->front()); // push the value to pop in the lua stack
+                                          // it will be the return value of the function in lua
+        (*ud)->pop_front(); // remove the value from the list
+        return 1; //we return 1 value in the stack
+    }
+}
+class Main
+{
+public:
+    Main();
+    ~Main();
+    void run();
+    
+    /* data */
+private:
+    lua_State *L;
+    std::list<int> theList;
+    void registerListType();
+    void runScript();
+};
+
+Main::Main() {
+    L = luaL_newstate();
+    luaL_openlibs(L);
 }
 
-using namespace std;
-int main()
+Main::~Main() {
+    lua_close(L);
+}
+
+void Main::runScript() {
+    lua_settop(L,0); //empty the lua stack
+    if(luaL_dofile(L, "./luascript.lua")) {
+        fprintf(stderr, "error: %s\n", lua_tostring(L,-1));
+        lua_pop(L,1);
+        exit(1);
+    }
+    assert(lua_gettop(L) == 0); //empty the lua stack
+}
+
+void Main::registerListType() {
+    std::cout << "Set the list object in lua" << std::endl;
+    luaL_newmetatable(L, "ListMT");
+    lua_pushvalue(L,-1);
+    lua_setfield(L,-2, "__index"); // ListMT .__index = ListMT
+    lua_pushcfunction(L, l_list_push);
+    lua_setfield(L,-2, "push"); // push in lua will call l_list_push in C++
+    lua_pushcfunction(L, l_list_pop);
+    lua_setfield(L,-2, "pop"); // pop in lua will call l_list_pop in C++
+}
+
+void Main::run() {
+    for(unsigned int i = 0; i<10; i++) // add some input data to the list
+        theList.push_back(i*100);
+    registerListType();
+    std::cout << "creating an instance of std::list in lua" << std::endl;
+    std::list<int> **ud = static_cast<std::list<int> **>(lua_newuserdata(L, sizeof(std::list<int> *)));
+    *(ud) = &theList;
+    luaL_setmetatable(L, "ListMT"); // set userdata metatable
+    lua_setglobal(L, "the_list"); // the_list in lua points to the new userdata
+    
+    runScript();
+    
+    while(!theList.empty()) { // read the data that lua left in the list
+        std::cout << "from C++: pop value " << theList.front() << std::endl;
+        theList.pop_front();
+    }
+    
+    
+}
+
+int main(int argc, char const *argv[])
 {
-  cout << "** Test Lua embedding" << endl;
-  cout << "** Init Lua" << endl;
-  lua_State *L;
-  L = luaL_newstate();
-  cout << "** Load the (optional) standard libraries, to have the print function" << endl;
-  luaL_openlibs(L);
-  cout << "** Load chunk. without executing it" << endl;
-  if (luaL_loadfile(L, "luascript.lua")) {
-    cerr << "Something went wrong loading the chunk (syntax error?)" << endl;
-    cerr << lua_tostring(L, -1) << endl;
-    lua_pop(L,1);
-  }
-
-  cout << "** Make a insert a global var into Lua from C++" << endl;
-  lua_pushnumber(L, 1.1);
-  lua_setglobal(L, "cppvar");
-
-  cout << "** Execute the Lua chunk" << endl;
-  if (lua_pcall(L,0, LUA_MULTRET, 0)) {
-    cerr << "Something went wrong during execution" << endl;
-    cerr << lua_tostring(L, -1) << endl;
-    lua_pop(L,1);
-  }
-  
-  cout << "** Read a global var from Lua into C++" << endl;
-  lua_getglobal(L, "luavar");
-  double luavar = lua_tonumber(L,-1);
-  lua_pop(L,1);
-  cout << "C++ can read the value set from Lua luavar = " << luavar << endl;
-
-  cout << "** Execute a Lua function from C++" << endl;
-  lua_getglobal(L, "myluafunction");
-  lua_pushnumber(L, 5);
-  lua_pcall(L, 1, 1, 0);
-  cout << "The return value of the function was " << lua_tostring(L, -1) << endl;
-  lua_pop(L,1);
-
-  cout << "** Execute a C++ function from Lua" << endl;
-  cout << "**** First register the function in Lua" << endl;
-  lua_pushcfunction(L,l_cppfunction);
-  lua_setglobal(L, "cppfunction");
-
-  cout << "**** Call a Lua function that uses the C++ function" << endl;
-  lua_getglobal(L, "myfunction");
-  lua_pushnumber(L, 5);
-  lua_pcall(L, 1, 1, 0);
-  cout << "The return value of the function was " << lua_tonumber(L, -1) << endl;
-  lua_pop(L,1);
-
-  cout << "** Release the Lua enviroment" << endl;
-  lua_close(L);
+    Main m;
+    m.run();
+    return 0;
 }
