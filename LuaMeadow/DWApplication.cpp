0b1847b692772e6010a8e6300964e5d2b6064f59
Manfred Endres <manfred.endres@tslarusso.de>
2013-08-25 19:26:27 +0200

    move lua c code into DWApplication implementation

--- LuaMeadow/DWApplication.cpp
+++ LuaMeadow/DWApplication.cpp
@@ -8,6 +8,32 @@
 
 #include "DWApplication.h"
 
+#pragma mark - LUA stack manipulation
+
+extern "C" {
+    static int l_list_push(lua_State *L) { // Push elements from LUA
+        assert(lua_gettop(L) == 2); // check that the number of args is exactly 2
+        std::list<int> **ud = static_cast<std::list<int> **>(luaL_checkudata(L,1, "ListMT")); // first arg is the list
+        int v =luaL_checkint(L,2); // seconds argument is the integer to be pushed to the std::list<int>
+        (*ud)->push_back(v); // perform the push on C++ object through the pointer stored in user data
+        return 0; // we return 0 values in the lua stack
+    }
+    static int l_list_pop(lua_State *L) {
+        assert(lua_gettop(L) == 1); // check that the number of args is exactly 1
+        std::list<int> **ud = static_cast<std::list<int> **>(luaL_checkudata(L, 1, "ListMT")); // first arg is the userdata
+        if ((*ud)->empty()) {
+            lua_pushnil(L);
+            return 1; // if list is empty the function will return nil
+        }
+        lua_pushnumber(L,(*ud)->front()); // push the value to pop in the lua stack
+                                          // it will be the return value of the function in lua
+        (*ud)->pop_front(); // remove the value from the list
+        return 1; //we return 1 value in the stack
+    }
+}
+
+
+
 DWApplication::DWApplication() {
     L = luaL_newstate();
     luaL_openlibs(L);
--- LuaMeadow/luascript.lua
+++ LuaMeadow/luascript.lua
@@ -1,13 +1,13 @@
 function entries(arg) -- iterator
-return function()
-return arg:pop();
-end
+    return function()
+        return arg:pop();
+    end
 end
 
 for i in entries(the_list) do
-io.write("From LUA:  ", i, "\n")
+    io.write("From LUA:  ", i, "\n")
 end
 
 for i=1,10 do
-the_list:push(50+i*100);
+    the_list:push(50+i*100);
 end
